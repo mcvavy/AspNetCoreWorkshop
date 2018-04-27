87c66b064a93a47e09adab49db32ca20fca0539f
Damian Kozlowski <Damian.Kozlowski@bglgroup.co.uk>
2018-04-27 15:59:25 +0100

    add middleware testing via TestServer

--- Workshop.Tests/StartupTests.cs
+++ Workshop.Tests/StartupTests.cs
@@ -11,7 +11,16 @@ namespace Workshop.Tests
         [Test]
         public async Task ShouldHaveSwaggerEndpoint()
         {
-            //???
+            var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            using (var client = testServer.CreateClient())
+            {
+                var response = await client.GetAsync("/swagger/v1/swagger.json");
+
+                response.EnsureSuccessStatusCode();
+
+                response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+            }
         }
     }
 }
