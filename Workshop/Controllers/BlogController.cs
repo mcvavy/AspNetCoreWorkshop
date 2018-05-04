fb37ac233ba2792492f6451e3a48552efd8ab765
Damian Kozlowski <Damian.Kozlowski@bglgroup.co.uk>
2018-05-04 15:41:51 +0100

    add blank post method

--- Workshop/Controllers/BlogController.cs
+++ Workshop/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Workshop.Entities;
@@ -18,6 +19,15 @@ namespace Workshop.Controllers
         public IActionResult Get()
         {
             return Ok("MVC is working !!!");
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Blog), 200)]
+        [ProducesResponseType(400)]
+        [Produces("application/json")]
+        public IActionResult Post([FromBody] Blog blog)
+        {
+            return Ok();
         }
     }
 }
